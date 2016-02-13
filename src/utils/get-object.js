cca8dd5d8342a8435d2fda67f8556e77a2949616
bertolomych <rom.bekk@gmail.com>
2016-02-13 22:06:36 +0100

    isPointInStroke fix

--- src/utils/get-object.js
+++ src/utils/get-object.js
@@ -1,11 +1,15 @@
-import {findLast} from 'lodash';
+import {findLast, get} from 'lodash';
 
 export default function getObject(x, y){
 	return findLast(
 		this.pathObjects,
 		pathObject => {
 			if(pathObject.rootNode) return true;
-			return this.ctx.isPointInPath(pathObject.path, x, y); // TODO add isPointInStroke
+			const fill = get(pathObject, 'node.style.fill');
+			const stroke = get(pathObject, 'node.style.stroke');
+			if(fill || !stroke) return this.ctx.isPointInPath(pathObject.path, x, y);
+			this.ctx.lineWidth = 6;
+			return this.ctx.isPointInStroke(pathObject.path, x, y);
 		}
 	);
 }
